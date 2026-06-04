c7f50191099a0e3585677c42973297a59c28728c
BMRahardja <6182301062@student.unpar.ac.id>
2026-06-04 18:16:30 +0700

    Update Main.java

--- Main/Main.java
+++ Main/Main.java
@@ -6,7 +6,8 @@ import RDA.RDA;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Algoritma Red Deer (RDA) untuk minimasi Total Flow Time");
+        System.out.println("Algoritma Red Deer (RDA) multi‑objektif (Rajendran)");
+        System.out.println("Meminimasi Makespan + Total Flow Time");
 
         System.out.print("Nama file yang dijalankan: ");
         String lokasiFile = sc.nextLine();
@@ -15,8 +16,6 @@ public class Main {
 
         TestRead kasusTaillard = new TestRead(lokasiFile);
         int[][] jadwalSoal = kasusTaillard.getKumpulanSoal()[soalTerpilih].getSoal();
-        Individu pemenang = null;
-        int nilaiObjektifPemenang = 0;
 
         System.out.print("Jumlah Populasi (NPop): ");
         int nPop = Integer.parseInt(sc.nextLine());
@@ -24,7 +23,7 @@ public class Main {
         int nStags = Integer.parseInt(sc.nextLine());
         System.out.print("Max Generasi: ");
         int maxGen = Integer.parseInt(sc.nextLine());
-        System.out.print("Intensitas Roaring (jumlah local search moves): ");
+        System.out.print("Intensitas Roaring (local search moves): ");
         int roarMoves = Integer.parseInt(sc.nextLine());
         System.out.print("Probabilitas Crossover (0-1): ");
         double crossoverProb = Double.parseDouble(sc.nextLine());
@@ -34,35 +33,28 @@ public class Main {
         RDA rda = new RDA(jadwalSoal, nPop, nStags, maxGen,
                           roarMoves, crossoverProb, mutationProb);
 
-        pemenang = new Individu(rda.getBestPermutation(), jadwalSoal.length);
-        nilaiObjektifPemenang = rda.getBestFlowTime();
-
-        Soal tampungSoal = new Soal(jadwalSoal.length, jadwalSoal[0].length);
-        int[][] urutanKerja = new int[jadwalSoal.length][jadwalSoal[0].length];
-        int[] urutanJob = pemenang.getUrutanPekerjaan();
-        for (int j = 0; j < jadwalSoal.length; j++) {
-            for (int k = 0; k < urutanJob.length; k++) {
-                urutanKerja[j][k] = jadwalSoal[j][urutanJob[k] - 1];
-            }
-        }
-        tampungSoal.setSoal(urutanKerja);
+        int[] bestJobs = rda.getBestPermutation();
+        int bestMS   = rda.getBestMakespan();
+        int bestTF   = rda.getBestTotalFlowTime();
 
         System.out.println("\n========== HASIL ==========");
         System.out.print("Urutan pekerjaan terbaik: ");
-        for (int j : urutanJob) System.out.print(j + " ");
+        for (int j : bestJobs) System.out.print(j + " ");
 
+        // Optional: display processing matrix ordered by this permutation
         System.out.println("\n\nJadwal matriks (mesin x pekerjaan):");
-        int[][] jadwal = tampungSoal.getSoal();
-        for (int i = 0; i < jadwal.length; i++) {
-            for (int j = 0; j < jadwal[0].length; j++) {
-                System.out.print(jadwal[i][j] + "\t");
+        for (int i = 0; i < jadwalSoal.length; i++) {
+            for (int k = 0; k < bestJobs.length; k++) {
+                System.out.print(jadwalSoal[i][bestJobs[k] - 1] + "\t");
             }
             System.out.println();
         }
 
-        System.out.println("\nTotal Flow Time Terbaik: " + nilaiObjektifPemenang);
-        System.out.println("Batas atas             : " + kasusTaillard.getBatasAtas()[soalTerpilih]);
-        System.out.println("Batas bawah            : " + kasusTaillard.getBatasBawah()[soalTerpilih]);
+        System.out.println("\nMakespan terbaik  : " + bestMS);
+        System.out.println("Total Flow Time   : " + bestTF);
+        System.out.println("Combined Fitness  : " + rda.getBestCombinedFitness());
+        System.out.println("Batas atas (TF)   : " + kasusTaillard.getBatasAtas()[soalTerpilih]);
+        System.out.println("Batas bawah (TF)  : " + kasusTaillard.getBatasBawah()[soalTerpilih]);
 
         sc.close();
     }
